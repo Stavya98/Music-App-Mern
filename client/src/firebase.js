1b4e55e4fe79e4592fb5b9d74c38b7100d78ce1f
Stavya Joshi <joshistavya@gmail.com>
2025-03-16 17:17:43 +0530

    updated enviroment variables

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDp_YeFROaJ-VeOCWtNSJ80m6bgmU8RiG0",
-  authDomain: "music-app-demo-7da7c.firebaseapp.com",
-  projectId: "music-app-demo-7da7c",
-  storageBucket: "music-app-demo-7da7c.firebasestorage.app",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
   messagingSenderId: "368136137636",
-  appId: "1:368136137636:web:6c57b60b137e18cce26f31",
-  measurementId: "G-TLJYX9LWHM"
+  appId: messagingSenderId,
+  measurementId: measurementId
 };
 
 // Initialize Firebase
